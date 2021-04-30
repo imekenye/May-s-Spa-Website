a4cb75b29df8ce957e68bfa1f61bc0c79194f6c9
imekenye <ismekenye@gmail.com>
2021-04-30 20:49:39 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    title: "gatsby-contentful-boiler",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "",
+        spaceId: "",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
